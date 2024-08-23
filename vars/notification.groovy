cd487c7cc67f2f416f3c296de3ecdf0d6cc995d4
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-08-23 19:58:02 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,5 +1,6 @@
 def call() {
     stage('notify') {
           emailext body: 'success', subject: 'Status', to: 'kondapratiwarpritam855@gmail.com'
+          slackSend channel: 'jenkinnotify', message: 'successfull '
     }
 }
